f513ce9e5bf789db5a7545f0510e2c99544e0618
dillondesilva <dillon.desilva@geeksdiary.com>
2023-09-20 14:16:55 +1000

    initial commit

---
+++ src/SensorControl.cpp
@@ -0,0 +1,20 @@
+class SensorControl {
+    public:
+        int btn1;
+        int btn2;
+        int btn3;
+
+        int led1;
+        int led2;
+        int led3;
+        
+        SensorControl(int btn1_pin, int btn2_pin, int btn3_pin, int led1_pin, int led2_pin, int led3_pin) {
+            btn1 = btn1_pin; 
+            btn2 = btn2_pin; 
+            btn3 = btn3_pin; 
+
+            btn1 = btn1_pin; 
+            btn1 = btn1_pin; 
+            btn1 = btn1_pin; 
+        }
+};
