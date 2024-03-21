f9b3be48ea14166454326ae2aa9aff909bd096ce
Dillon de Silva <dillon.desilva@geeksdiary.com>
2024-03-22 10:53:15 +1100

    Delete build directory

--- build/sketch/switch.cpp.d
+++
@@ -1,3 +0,0 @@
-/Users/dillon/projects/esp32-ppg-system/build/sketch/switch.cpp.o: \
- /Users/dillon/projects/esp32-ppg-system/build/sketch/switch.cpp \
- /Users/dillon/projects/esp32-ppg-system/build/sketch/switch.h
